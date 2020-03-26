91ec538ddb38497ed76094a63c95e0e96e869046
larrybechtel <54470955+larrybechtel@users.noreply.github.com>
2020-03-26 21:46:10 +0100

    Add files via upload

---
+++ javascript_assign.js
@@ -0,0 +1,16 @@
+let id="Ibrahim Ajala";
+let subject=[html,css,js];
+
+console.log("My name is: " + id);
+console.log("My courses are: " + subject);
+const data = [];
+for (let i=0; i<=200; i++) {
+
+if (i%2===1) {
+
+data.push(i);
+ }
+
+}
+
+console.log(data);
